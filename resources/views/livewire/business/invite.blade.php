1f586ddb524ed83721237228e52e07c3cf610034
meshgenerali <meshgenerali@kali>
2024-10-23 07:23:55 +0300

    invited users lists and resend button

--- resources/views/livewire/business/invite.blade.php
+++ resources/views/livewire/business/invite.blade.php
@@ -2,6 +2,46 @@
 <div>
 
     <x-button wire:click="invite">Invite User</x-button>
+    <br> <br>
+    
+    <div class="bg-gray-700 text-white text-2xl">
+        <div class="container mx-auto px-4 py-8">
+            <div class="overflow-x-auto">
+                <table class="min-w-full bg-gray-800 text-white">
+                    <thead class="bg-gray-700">
+                        <tr>
+                            <th class="py-2 px-4 text-left">#</th>
+                            <th class="py-2 px-4 text-left">Email</th>
+                            <th class="py-2 px-4 text-left">Sent At</th>
+                            <th class="py-2 px-4 text-left">Actions</th>
+                        </tr>
+                    </thead>
+
+                    @php
+                    $index_serial = ($invitations->currentPage()-1) * $invitations->perPage()+1;
+                    @endphp
+                    
+                    <tbody class="bg-gray-800 divide-y divide-gray-700">
+                        @foreach ($invitations as $invitation)
+                        
+                        <tr>
+                            <td class="py-2 px-4">{{$index_serial++}}</td>
+                            <td class="py-2 px-4">{{$invitation->email}}</td>
+                            <td class="py-2 px-4">{{$invitation->created_at}}</td>
+                            <td class="py-2 px-4">
+                                <x-secondary-button wire:click="resend('{{$invitation->email}}')">Resend</x-secondary-button>
+                            </td>
+                        </tr>
+
+                        @endforeach
+                  
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+        
+    </div>
 
 <x-dialog-modal wire:model="inviteModal">
     <!-- <x-slot name="title">
